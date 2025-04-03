8c9ff89a6d2513a2b841ec60d8bbfed49e8de93b
Oluwatoyinah <akinbobolaoluwatoyin25@gmail.com>
2025-04-03 11:45:53 +0100

    feat: spinner added

--- src/pages/Revenue.tsx
+++ src/pages/Revenue.tsx
@@ -1,6 +1,7 @@
 import WalletBox from "@components/dashboard-view/cards/WalletBox";
 import { DashboardChart } from "@components/dashboard-view/charts/DashboardChart";
 import CustomButton from "@components/dashboard-view/shared/CustomButton";
+import Spinner from "@components/dashboard-view/shared/Spinner";
 import FilterDrawer from "@components/sections/filter/FilterDrawer";
 import Transaction from "@components/sections/transactions/Transaction";
 import { DataProvider } from "@context/data-context";
@@ -60,48 +61,54 @@ const Revenue = () => {
   return (
     <>
       <DataProvider>
-        <div>
-          <div className="w-full flex gap-16 mb-12">
-            <div className="w-3/4 flex flex-col gap-5">
-              <div className="flex items-center gap-16 flex-wrap">
-                <div>
-                  <div className="flex gap-2 justify-between items-center mb-3">
-                    <p className="tracking-[0.4px] leading-[16px] text-[14px]">Available Balance</p>
-                  </div>
-
-                  <h2 className="font-bold text-[36px] leading-[48px]">USD {BALANCE_DATA?.value || "0.00"}</h2>
-                </div>
-                <div>
-                  <CustomButton className="rounded-full">
-                    <span className="p-3 text-[13px]">Withdraw</span>
-                  </CustomButton>
-                </div>
-              </div>
-
-              <div className="min-h-10 flex-grow ">
-                <DashboardChart />
-              </div>
-            </div>
-            <div className="w-1/4">
-              {!isFetchingWallet &&
-                WALLET_ARR &&
-                WALLET_ARR?.filter(Boolean)?.length !== 0 &&
-                WALLET_ARR?.filter((_) => _?.title !== "Balance")?.map((el, i) => (
-                  <div className="mb-10" key={`${el?.title} ${i}`}>
-                    <WalletBox title={el?.title} price_value={el?.value} />
-                  </div>
-                ))}
-            </div>
-          </div>
-
+        {isFetchingTransactions || isFetchingWallet ? (
           <div>
-            <Transaction
-              data={ALL_TRANSACTIONS}
-              isFetchingTransaction={isFetchingTransactions}
-              openDrawer={() => setOpenFilterDrawer(true)}
-            />
+            <Spinner />
           </div>
-        </div>
+        ) : (
+          <div>
+            <div className="w-full flex gap-16 mb-12">
+              <div className="w-3/4 flex flex-col gap-5">
+                <div className="flex items-center gap-16 flex-wrap">
+                  <div>
+                    <div className="flex gap-2 justify-between items-center mb-3">
+                      <p className="tracking-[0.4px] leading-[16px] text-[14px]">Available Balance</p>
+                    </div>
+
+                    <h2 className="font-bold text-[36px] leading-[48px]">USD {BALANCE_DATA?.value || "0.00"}</h2>
+                  </div>
+                  <div>
+                    <CustomButton className="rounded-full">
+                      <span className="p-3 text-[13px]">Withdraw</span>
+                    </CustomButton>
+                  </div>
+                </div>
+
+                <div className="min-h-10 flex-grow ">
+                  <DashboardChart />
+                </div>
+              </div>
+              <div className="w-1/4">
+                {!isFetchingWallet &&
+                  WALLET_ARR &&
+                  WALLET_ARR?.filter(Boolean)?.length !== 0 &&
+                  WALLET_ARR?.filter((_) => _?.title !== "Balance")?.map((el, i) => (
+                    <div className="mb-10" key={`${el?.title} ${i}`}>
+                      <WalletBox title={el?.title} price_value={el?.value} />
+                    </div>
+                  ))}
+              </div>
+            </div>
+
+            <div>
+              <Transaction
+                data={ALL_TRANSACTIONS}
+                isFetchingTransaction={isFetchingTransactions}
+                openDrawer={() => setOpenFilterDrawer(true)}
+              />
+            </div>
+          </div>
+        )}
         <FilterDrawer active={openFilterDrawer} closeDrawer={() => setOpenFilterDrawer(false)} />
       </DataProvider>
     </>
